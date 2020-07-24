ead35fb4e8dbe56965e580460829e0dc7d27f8f4
Lahcen Anjaimi <admin@MacBook.local>
2020-07-24 02:59:42 -0400

    ss

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use('/api/random', randomRouter);
 
 app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+}); */
 
 const PORT = process.env.PORT || 8080;
 
