e808b5859bb8ef62bd88c55b9797e49c4e5c1896
Lahcen Anjaimi <admin@MacBook.local>
2020-07-28 10:48:40 -0400

    added build

---
+++ client/build/precache-manifest.5d18bd95f1962aa31d52b6f9b0d95d0d.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c4683c6dd3fd462006845e20ffbe6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "032104284027d84c20ec",
+    "url": "/static/css/2.667e231a.chunk.css"
+  },
+  {
+    "revision": "92da5116f7bc0c4e31bb",
+    "url": "/static/css/main.5a3c4a4c.chunk.css"
+  },
+  {
+    "revision": "032104284027d84c20ec",
+    "url": "/static/js/2.2e97c4e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e97c4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92da5116f7bc0c4e31bb",
+    "url": "/static/js/main.a65643a8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dd1d282e68d6e161dfed38037bc70b7e",
+    "url": "/static/media/Appliances.dd1d282e.png"
+  },
+  {
+    "revision": "324ecaf0b9fae6afb5c0eda7e5298920",
+    "url": "/static/media/Boats.324ecaf0.png"
+  },
+  {
+    "revision": "b888697d4231ed6804a44b1271a2e96e",
+    "url": "/static/media/Cars.b888697d.png"
+  },
+  {
+    "revision": "2f90a663713a95ef33c0f3091b584405",
+    "url": "/static/media/Clothes.2f90a663.png"
+  },
+  {
+    "revision": "aed3ee7b92d7511bb2a4f10780ebd403",
+    "url": "/static/media/Collectibles.aed3ee7b.png"
+  },
+  {
+    "revision": "dabc024263af60b4290661ebcf91ce16",
+    "url": "/static/media/Electronics.dabc0242.png"
+  },
+  {
+    "revision": "5d06b827a3caf493b959fa65224e71b1",
+    "url": "/static/media/Furniture.5d06b827.png"
+  },
+  {
+    "revision": "936a596d453ed8fc0ef1620def76b9c2",
+    "url": "/static/media/Laptops.936a596d.png"
+  },
+  {
+    "revision": "c798f53f7c99a930641fa4666cd60f75",
+    "url": "/static/media/Motocycles.c798f53f.png"
+  },
+  {
+    "revision": "95bf2576ab8516bc7c3065e3f7285b30",
+    "url": "/static/media/Phones.95bf2576.png"
+  },
+  {
+    "revision": "d6ed483a37eb8168d805b7336bbefe29",
+    "url": "/static/media/Shoes.d6ed483a.png"
+  },
+  {
+    "revision": "3ce481cc85375f89b7179eb3c507a41d",
+    "url": "/static/media/Toys.3ce481cc.png"
+  },
+  {
+    "revision": "5f0fa2437eb48fe97d714f1bc3475ceb",
+    "url": "/static/media/boat.5f0fa243.png"
+  },
+  {
+    "revision": "da411d7ae4ccf73a4a0996fb8b2b4afe",
+    "url": "/static/media/fur.da411d7a.png"
+  },
+  {
+    "revision": "1c5d36ba3cb93a058b1dc4c4c5b6d8f9",
+    "url": "/static/media/iphone.1c5d36ba.png"
+  },
+  {
+    "revision": "4fe1451938bdbc38dd1a6af0834ade0f",
+    "url": "/static/media/laptop.4fe14519.png"
+  },
+  {
+    "revision": "4bd66cd716eefed7a3defc242c8f61d2",
+    "url": "/static/media/logo_green.4bd66cd7.png"
+  },
+  {
+    "revision": "746c2e6a4aa9f19d6438deb12d12f3ef",
+    "url": "/static/media/logo_white.746c2e6a.png"
+  },
+  {
+    "revision": "519520a14d17da6cf6551aebb2a38ab5",
+    "url": "/static/media/phone.519520a1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "839b2d7ee5f408b91c120c7b74610bdd",
+    "url": "/static/media/surf.839b2d7e.png"
+  },
+  {
+    "revision": "376cc5b646ca6f40509e3d4a0264023b",
+    "url": "/static/media/tools.376cc5b6.png"
+  }
+]);
